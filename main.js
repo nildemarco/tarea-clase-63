ee6c2f63bdce7efc50126518b5354766dff1a0cd
nildemarco <nildemarco@gmail.com>
2020-01-17 21:24:45 -0300

    Fin de la tarea

---
+++ main.js
@@ -0,0 +1,73 @@
+const imagesAsObjects = [
+    {
+        title: "Forest",
+        img: "https://www.w3schools.com/css/img_forest.jpg"
+    },
+    {
+        title: "5 terre",
+        img: "https://www.w3schools.com/css/img_5terre.jpg"
+    },
+    {
+        title: "Northern lights",
+        img: "https://www.w3schools.com/css/img_lights.jpg"
+    },
+    {
+        title: "Mountains",
+        img: "https://www.w3schools.com/css/img_mountains.jpg"
+    }
+]
+
+let acumuladoraCards = "";
+
+
+for (let i = 0; i < imagesAsObjects.length; i++) {
+    acumuladoraCards += `<div class="card">
+    <img src=${imagesAsObjects[i].img} alt=${imagesAsObjects[i].title}>
+  <div class="desc">${imagesAsObjects[i].title}</div>
+  </div>`;
+//   <div id="myModal" class="modal nomostrar">
+//         <div class="modalcontent">
+//           <img src=${imagesAsObjects[i].img} alt=${imagesAsObjects[i].title}>
+//           <div class="desc">${imagesAsObjects[i].title}</div>
+//           <button id="cerrarModal">Cerrar Modal</button>
+//         </div>
+//     </div>;
+  
+}
+
+const containerHtml = document.getElementById("container");
+
+containerHtml.innerHTML = acumuladoraCards;
+
+
+const modal = document.getElementById("myModal");
+
+const botonAbrirModal = document.getElementsByTagName("img");
+const modalcontenido = document.querySelector(".modalcontent")
+
+
+
+for (let i = 0; i < botonAbrirModal.length; i++) {
+    botonAbrirModal[i].onclick = () => {
+        console.log(`Cualquier cosa`)
+        modal.classList.remove("nomostrar")
+        
+        modalcontenido.innerHTML = `
+        <img src=${imagesAsObjects[i].img} alt=${imagesAsObjects[i].title}>
+        <div class="desc">${imagesAsObjects[i].title}</div>
+        `
+    }
+    
+}
+const botonCerrarModal = document.getElementById("cerrarModal")
+
+
+botonCerrarModal.onclick = () => {
+    console.log(`Aprete boton`)
+    modal.classList.add("nomostrar")
+}
+
+
+
+
+
